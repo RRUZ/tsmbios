89b35d1bb8f8200a1c0a326892c48fd951633a78
Rodrigo.Ruz.V@gmail.com <Rodrigo.Ruz.V@gmail.com@ac957637-0643-cdac-08f5-774b64107b9e>
2013-01-17 15:04:22 +0000

    Added TCacheInfo record Updated TProcessorInfo record Updated Processor Info Sample project

--- trunk/Samples/Table 4 Processor Information/ProcessorInformation.dpr
+++ trunk/Samples/Table 4 Processor Information/ProcessorInformation.dpr
@@ -9,12 +9,51 @@ uses
   SysUtils,
   ActiveX,
   ComObj,
+  TypInfo,
   uSMBIOS in '..\..\Common\uSMBIOS.pas';
+
+//http://tondrej.blogspot.com/2007/10/settostring-stringtoset.html
+function GetOrdValue(Info: PTypeInfo; const SetParam): Integer;
+begin
+  Result := 0;
+
+  case GetTypeData(Info)^.OrdType of
+    otSByte, otUByte:
+      Result := Byte(SetParam);
+    otSWord, otUWord:
+      Result := Word(SetParam);
+    otSLong, otULong:
+      Result := Integer(SetParam);
+  end;
+end;
+
+
+function SetToString(Info: PTypeInfo; const SetParam; Brackets: Boolean): AnsiString;
+var
+  S: TIntegerSet;
+  TypeInfo: PTypeInfo;
+  I: Integer;
+begin
+  Result := '';
+
+  Integer(S) := GetOrdValue(Info, SetParam);
+  TypeInfo := GetTypeData(Info)^.CompType^;
+  for I := 0 to SizeOf(Integer) * 8 - 1 do
+    if I in S then
+    begin
+      if Result <> '' then
+        Result := Result + ',';
+      Result := Result + GetEnumName(TypeInfo, I);
+    end;
+  if Brackets then
+    Result := '[' + Result + ']';
+end;
 
 procedure GetEnclosureInfo;
 Var
   SMBios             : TSMBios;
   LProcessorInfo     : TProcessorInfo;
+  LSRAMTypes         : TCacheSRAMTypes;
 begin
   SMBios:=TSMBios.Create;
   try
@@ -34,9 +73,6 @@ begin
         WriteLn(Format('Current processor speed %d  Mhz',[LProcessorInfo.CurrentSpeed]));
         WriteLn('Processor Upgrade   '+LProcessorInfo.ProcessorUpgradeStr);
         WriteLn(Format('External Clock     %d  Mhz',[LProcessorInfo.ExternalClock]));
-        WriteLn(Format('L1 Cache Handle    %x',[LProcessorInfo.L1CacheHandle]));
-        WriteLn(Format('L2 Cache Handle    %x',[LProcessorInfo.L2CacheHandle]));
-        WriteLn(Format('L3 Cache Handle    %x',[LProcessorInfo.L3CacheHandle]));
         WriteLn('Serial Number      '+LProcessorInfo.SerialNumberStr);
         WriteLn('Asset Tag          '+LProcessorInfo.AssetTagStr);
         WriteLn('Part Number        '+LProcessorInfo.PartNumberStr);
@@ -44,8 +80,61 @@ begin
         WriteLn(Format('Cores Enabled      %d',[LProcessorInfo.CoreEnabled]));
         WriteLn(Format('Threads Count      %d',[LProcessorInfo.ThreadCount]));
         WriteLn(Format('Processor Characteristics %.4x',[LProcessorInfo.ProcessorCharacteristics]));
+        Writeln;
 
-        WriteLn;
+        if LProcessorInfo.L1CacheHandle>0 then
+        begin
+          WriteLn('L1 Cache Handle Info');
+          WriteLn('--------------------');
+          WriteLn('  Socket Designation    '+LProcessorInfo.L1Chache.SocketDesignationStr);
+          WriteLn(Format('  Cache Configuration   %.4x',[LProcessorInfo.L1Chache.CacheConfiguration]));
+          WriteLn(Format('  Maximum Cache Size    %d Kb',[LProcessorInfo.L1Chache.GetMaximumCacheSize]));
+          WriteLn(Format('  Installed Cache Size  %d Kb',[LProcessorInfo.L1Chache.GetInstalledCacheSize]));
+          LSRAMTypes:=LProcessorInfo.L1Chache.GetSupportedSRAMType;
+          WriteLn(Format('  Supported SRAM Type   %s',[SetToString(TypeInfo(TCacheSRAMTypes), LSRAMTypes, True)]));
+          LSRAMTypes:=LProcessorInfo.L1Chache.GetCurrentSRAMType;
+          WriteLn(Format('  Current SRAM Type     %s',[SetToString(TypeInfo(TCacheSRAMTypes), LSRAMTypes, True)]));
+
+          WriteLn(Format('  Error Correction Type %s',[ErrorCorrectionTypeStr[LProcessorInfo.L1Chache.GetErrorCorrectionType]]));
+          WriteLn(Format('  System Cache Type     %s',[SystemCacheTypeStr[LProcessorInfo.L1Chache.GetSystemCacheType]]));
+          WriteLn(Format('  Associativity         %s',[LProcessorInfo.L1Chache.AssociativityStr]));
+        end;
+
+        if LProcessorInfo.L2CacheHandle>0 then
+        begin
+          WriteLn('L2 Cache Handle Info');
+          WriteLn('--------------------');
+          WriteLn('  Socket Designation    '+LProcessorInfo.L2Chache.SocketDesignationStr);
+          WriteLn(Format('  Cache Configuration   %.4x',[LProcessorInfo.L2Chache.CacheConfiguration]));
+          WriteLn(Format('  Maximum Cache Size    %d Kb',[LProcessorInfo.L2Chache.GetMaximumCacheSize]));
+          WriteLn(Format('  Installed Cache Size  %d Kb',[LProcessorInfo.L2Chache.GetInstalledCacheSize]));
+          LSRAMTypes:=LProcessorInfo.L2Chache.GetSupportedSRAMType;
+          WriteLn(Format('  Supported SRAM Type   %s',[SetToString(TypeInfo(TCacheSRAMTypes), LSRAMTypes, True)]));
+          LSRAMTypes:=LProcessorInfo.L2Chache.GetCurrentSRAMType;
+          WriteLn(Format('  Current SRAM Type     %s',[SetToString(TypeInfo(TCacheSRAMTypes), LSRAMTypes, True)]));
+
+          WriteLn(Format('  Error Correction Type %s',[ErrorCorrectionTypeStr[LProcessorInfo.L2Chache.GetErrorCorrectionType]]));
+          WriteLn(Format('  System Cache Type     %s',[SystemCacheTypeStr[LProcessorInfo.L2Chache.GetSystemCacheType]]));
+          WriteLn(Format('  Associativity         %s',[LProcessorInfo.L2Chache.AssociativityStr]));
+        end;
+
+        if LProcessorInfo.L3CacheHandle>0 then
+        begin
+          WriteLn('L3 Cache Handle Info');
+          WriteLn('--------------------');
+          WriteLn('  Socket Designation    '+LProcessorInfo.L3Chache.SocketDesignationStr);
+          WriteLn(Format('  Cache Configuration   %.4x',[LProcessorInfo.L3Chache.CacheConfiguration]));
+          WriteLn(Format('  Maximum Cache Size    %d Kb',[LProcessorInfo.L3Chache.GetMaximumCacheSize]));
+          WriteLn(Format('  Installed Cache Size  %d Kb',[LProcessorInfo.L3Chache.GetInstalledCacheSize]));
+          LSRAMTypes:=LProcessorInfo.L3Chache.GetSupportedSRAMType;
+          WriteLn(Format('  Supported SRAM Type   %s',[SetToString(TypeInfo(TCacheSRAMTypes), LSRAMTypes, True)]));
+          LSRAMTypes:=LProcessorInfo.L3Chache.GetCurrentSRAMType;
+          WriteLn(Format('  Current SRAM Type     %s',[SetToString(TypeInfo(TCacheSRAMTypes), LSRAMTypes, True)]));
+
+          WriteLn(Format('  Error Correction Type %s',[ErrorCorrectionTypeStr[LProcessorInfo.L3Chache.GetErrorCorrectionType]]));
+          WriteLn(Format('  System Cache Type     %s',[SystemCacheTypeStr[LProcessorInfo.L3Chache.GetSystemCacheType]]));
+          WriteLn(Format('  Associativity         %s',[LProcessorInfo.L3Chache.AssociativityStr]));
+        end;
       end
       else
       Writeln('No Processor Info was found');
