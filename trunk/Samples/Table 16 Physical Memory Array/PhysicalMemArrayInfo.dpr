51daf121ce7992818ac77ea2f76c5ac19120cf15
Rodrigo.Ruz.V@gmail.com <Rodrigo.Ruz.V@gmail.com@ac957637-0643-cdac-08f5-774b64107b9e>
2013-01-22 01:31:16 +0000

    Added TPhysicalMemoryArrayInfo record (Table Type 16) Added sample project for TPhysicalMemoryArrayInfo

---
+++ trunk/Samples/Table 16 Physical Memory Array/PhysicalMemArrayInfo.dpr
@@ -0,0 +1,54 @@
+program PhysicalMemArrayInfo;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  Classes,
+  SysUtils,
+  uSMBIOS in '..\..\Common\uSMBIOS.pas';
+
+procedure GetPhysicalMemArrayInfo;
+Var
+  SMBios : TSMBios;
+  LPhysicalMemArr  : TPhysicalMemoryArrayInformation;
+begin
+  SMBios:=TSMBios.Create;
+  try
+      WriteLn('Physical Memory Array Information');
+      WriteLn('--------------------------------');
+      if SMBios.HasPhysicalMemoryArrayInfo then
+      for LPhysicalMemArr in SMBios.PhysicalMemoryArrayInfo do
+      begin
+        WriteLn('Location         '+LPhysicalMemArr.GetLocationStr);
+        WriteLn('Use              '+LPhysicalMemArr.GetUseStr);
+        WriteLn('Error Correction '+LPhysicalMemArr.GetErrorCorrectionStr);
+        if LPhysicalMemArr.RAWPhysicalMemoryArrayInformation.MaximumCapacity<>$80000000 then
+          WriteLn(Format('Maximum Capacity %d Kb',[LPhysicalMemArr.RAWPhysicalMemoryArrayInformation.MaximumCapacity]))
+        else
+          WriteLn(Format('Maximum Capacity %d bytes',[LPhysicalMemArr.RAWPhysicalMemoryArrayInformation.ExtendedMaximumCapacity]));
+
+        WriteLn(Format('Memory devices   %d',[LPhysicalMemArr.RAWPhysicalMemoryArrayInformation.NumberofMemoryDevices]));
+        WriteLn;
+      end
+      else
+
+
+      Writeln('No Physical Memory Array Info was found');
+  finally
+   SMBios.Free;
+  end;
+end;
+
+
+begin
+ try
+    GetPhysicalMemArrayInfo;
+ except
+    on E:Exception do
+        Writeln(E.Classname, ':', E.Message);
+ end;
+ Writeln('Press Enter to exit');
+ Readln;
+end.
