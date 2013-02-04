16076e8f48a47124b2e0aff7801cf89ec7334785
Rodrigo.Ruz.V@gmail.com <Rodrigo.Ruz.V@gmail.com@ac957637-0643-cdac-08f5-774b64107b9e>
2013-02-04 17:22:32 +0000

    Added Table 19 Memory Array Mapped Address record Added Table 19 Memory Array Mapped Address sample project

---
+++ trunk/Samples/Table 19 Memory Array Mapped Address/MemArrayMappedInfo.dpr
@@ -0,0 +1,55 @@
+program MemArrayMappedInfo;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  Classes,
+  SysUtils,
+  uSMBIOS in '..\..\Common\uSMBIOS.pas';
+
+procedure GetMemArrayMappedInfo;
+Var
+  SMBios : TSMBios;
+  LMemArrMappedAddress  : TMemoryArrayMappedAddressInformation;
+begin
+  SMBios:=TSMBios.Create;
+  try
+      WriteLn('Memory Array Mapped Address Information');
+      WriteLn('---------------------------------------');
+      if SMBios.HasMemoryArrayMappedAddressInfo then
+      for LMemArrMappedAddress in SMBios.MemoryArrayMappedAddressInformation do
+      begin
+        WriteLn(Format('Starting Address    %.8x ',[LMemArrMappedAddress.RAWMemoryArrayMappedAddressInfo.StartingAddress]));
+        WriteLn(Format('Ending   Address    %.8x ',[LMemArrMappedAddress.RAWMemoryArrayMappedAddressInfo.EndingAddress]));
+        WriteLn(Format('Memory Array Handle %.4x ',[LMemArrMappedAddress.RAWMemoryArrayMappedAddressInfo.MemoryArrayHandle]));
+        WriteLn(Format('Partition Width     %d ',[LMemArrMappedAddress.RAWMemoryArrayMappedAddressInfo.PartitionWidth]));
+        if SMBios.SmbiosVersion>='2.7' then
+        begin
+          WriteLn(Format('Extended Starting Address  %x',[LMemArrMappedAddress.RAWMemoryArrayMappedAddressInfo.ExtendedStartingAddress]));
+          WriteLn(Format('Extended Ending   Address  %x',[LMemArrMappedAddress.RAWMemoryArrayMappedAddressInfo.ExtendedEndingAddress]));
+        end;
+
+        WriteLn;
+      end
+      else
+
+
+      Writeln('No Memory Array Mapped Address Info was found');
+  finally
+   SMBios.Free;
+  end;
+end;
+
+
+begin
+ try
+    GetMemArrayMappedInfo;
+ except
+    on E:Exception do
+        Writeln(E.Classname, ':', E.Message);
+ end;
+ Writeln('Press Enter to exit');
+ Readln;
+end.
