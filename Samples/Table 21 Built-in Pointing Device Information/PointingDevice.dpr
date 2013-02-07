ba562603d6111fc7dcb59796fcd8060ac869b68e
Rodrigo.Ruz.V@gmail.com <Rodrigo.Ruz.V@gmail.com@ac957637-0643-cdac-08f5-774b64107b9e>
2013-02-07 04:12:19 +0000

    Added Built-in Pointing Device Information (Type 22) record Added Built-in Pointing Device Information sample project

---
+++ Samples/Table 21 Built-in Pointing Device Information/PointingDevice.dpr
@@ -0,0 +1,46 @@
+program PointingDevice;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  Classes,
+  SysUtils,
+  uSMBIOS in '..\..\Common\uSMBIOS.pas';
+
+procedure GetPointingDeviceInfo;
+Var
+  SMBios : TSMBios;
+  LPointDevice  : TBuiltInPointingDeviceInformation;
+begin
+  SMBios:=TSMBios.Create;
+  try
+      WriteLn('Built-in Pointing Device Information');
+      WriteLn('------------------------------------');
+      if SMBios.HasBuiltInPointingDeviceInfo then
+      for LPointDevice in SMBios.BuiltInPointingDeviceInformation do
+      begin
+        WriteLn(Format('Type              %s',[LPointDevice.GetType]));
+        WriteLn(Format('Interface         %s',[LPointDevice.GetInterface]));
+        WriteLn(Format('Number of Buttons %d',[LPointDevice.RAWBuiltInPointingDeviceInfo.NumberofButtons]));
+        WriteLn;
+      end
+      else
+      Writeln('No Built-in Pointing Device Info was found');
+  finally
+   SMBios.Free;
+  end;
+end;
+
+
+begin
+ try
+    GetPointingDeviceInfo;
+ except
+    on E:Exception do
+        Writeln(E.Classname, ':', E.Message);
+ end;
+ Writeln('Press Enter to exit');
+ Readln;
+end.
