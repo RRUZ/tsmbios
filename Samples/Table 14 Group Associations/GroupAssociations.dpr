fb779e6dd25555e06332027a2f17ddeec4ce2dd4
Rodrigo.Ruz.V@gmail.com <Rodrigo.Ruz.V@gmail.com@ac957637-0643-cdac-08f5-774b64107b9e>
2013-06-07 20:21:03 +0000

    Added support for Table 14 Group Associations Added Delphi sample project for Table 14 Group Associations

---
+++ Samples/Table 14 Group Associations/GroupAssociations.dpr
@@ -0,0 +1,46 @@
+program  GroupAssociations;
+
+{$APPTYPE CONSOLE}
+
+uses
+  Classes,
+  SysUtils,
+  uSMBIOS in '..\..\Common\uSMBIOS.pas';
+
+procedure GetGroupAssociationsInfo;
+Var
+  SMBios : TSMBios;
+  LGroup : TGroupAssociationsInformation;
+begin
+  SMBios:=TSMBios.Create;
+  try
+      SMBios.FindAndLoadFromFile('C:\Users\Dexter\Desktop\RAD Studio Projects\google-code\SMBIOS Delphi\Docs\DELL_system_dumps\PE2450\SMBIOS.dat');
+      WriteLn('Group Associations Information');
+      WriteLn('------------------------------');
+      if SMBios.HasGroupAssociationsInfo then
+      for LGroup in SMBios.GroupAssociationsInformation do
+      begin
+        WriteLn('Group Name    '+LGroup.GetGroupName);
+        WriteLn('Item Type     '+IntToStr(LGroup.RAWGroupAssociationsInformation.ItemType));
+        WriteLn('Item Handle   '+IntToStr(LGroup.RAWGroupAssociationsInformation.ItemHandle));
+        WriteLn;
+      end
+      else
+      Writeln('No Group Associations Info was found');
+  finally
+   SMBios.Free;
+  end;
+end;
+
+
+begin
+ try
+    GetGroupAssociationsInfo;
+ except
+    on E:Exception do
+        Writeln(E.Classname, ':', E.Message);
+ end;
+ Writeln('Press Enter to exit');
+ Readln;
+end.
+
