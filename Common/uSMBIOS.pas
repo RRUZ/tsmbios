28abe50b7c5a7354032c208e70ba16c3dcbc9641
Rodrigo.Ruz.V@gmail.com <Rodrigo.Ruz.V@gmail.com@ac957637-0643-cdac-08f5-774b64107b9e>
2014-04-10 16:47:48 +0000

    updated copyright small fix

--- Common/uSMBIOS.pas
+++ Common/uSMBIOS.pas
@@ -1,23 +1,23 @@
-{**************************************************************************************************}
-{                                                                                                  }
-{ Unit uSMBIOS                                                                                     }
-{ unit for the TSMBIOS                                                                             }
-{                                                                                                  }
-{ The contents of this file are subject to the Mozilla Public License Version 1.1 (the "License"); }
-{ you may not use this file except in compliance with the License. You may obtain a copy of the    }
-{ License at http://www.mozilla.org/MPL/                                                           }
-{                                                                                                  }
-{ Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF   }
-{ ANY KIND, either express or implied. See the License for the specific language governing rights  }
-{ and limitations under the License.                                                               }
-{                                                                                                  }
-{ The Original Code is uSMBIOS.pas.                                                                }
-{                                                                                                  }
-{ The Initial Developer of the Original Code is Rodrigo Ruz V.                                     }
-{ Portions created by Rodrigo Ruz V. are Copyright (C) 2012-2013 Rodrigo Ruz V.                    }
-{ All Rights Reserved.                                                                             }
-{                                                                                                  }
-{**************************************************************************************************}
+//**************************************************************************************************
+//
+// Unit uSMBIOS
+// unit for the TSMBIOS Project https://code.google.com/p/tsmbios/
+//
+// The contents of this file are subject to the Mozilla Public License Version 1.1 (the "License");
+// you may not use this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.mozilla.org/MPL/
+//
+// Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
+// ANY KIND, either express or implied. See the License for the specific language governing rights
+// and limitations under the License.
+//
+// The Original Code is uSMBIOS.pas.
+//
+// The Initial Developer of the Original Code is Rodrigo Ruz V.
+// Portions created by Rodrigo Ruz V. are Copyright (C) 2012-2014 Rodrigo Ruz V.
+// All Rights Reserved.
+//
+//**************************************************************************************************
 unit uSMBIOS;
 
 interface
@@ -5356,7 +5356,7 @@ begin
 
   SetLength(FSystemSlotInfo, GetSMBiosTableEntries(SystemSlotsInformation));
   i:=0;
-  LIndex:=0;
+  LIndex:=-1;
   if Length(FSystemSlotInfo)>0 then
   repeat
     LIndex := GetSMBiosTableNextIndex(SystemSlotsInformation, LIndex);
